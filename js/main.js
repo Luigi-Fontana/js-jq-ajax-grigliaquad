4b364de22a2727ff93b859238ebf56a460e5adae
Luigi Fontana <luigifontana92@gmail.com>
2020-03-16 15:52:09 +0100

    Svolgimento esercizio senza Handlebars

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
-
+$(document).ready(function(){
+    $('.square').click(function(){
+        var that = $(this);
+        $.ajax({
+            url: 'https://flynn.boolean.careers/exercises/api/random/int',
+            method: 'GET',
+            success: function (numero) {
+                var random = numero.response;
+                that.text(random);
+                if (random > 5) {
+                    that.addClass('green');
+                } else {
+                    that.addClass('orange');
+                }
+            },
+            error: function () {
+                alert('Errore Generico');
+            }
+        });
+    });
+});
